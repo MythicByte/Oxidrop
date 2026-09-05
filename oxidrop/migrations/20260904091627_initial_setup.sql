e2aee6eef4bc7a4ab705dbde4844feb2c2779fda
Markus Götz <mgmarkusgoetz@gmail.com>
2026-09-05 02:37:29 +0200

    Db: add user salt field

--- oxidrop/migrations/20260904091627_initial_setup.sql
+++ oxidrop/migrations/20260904091627_initial_setup.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
+    password_salt TEXT NOT NULL,
     
     -- Roles
     role TEXT NOT NULL DEFAULT 'viewer' CHECK(role IN ('admin', 'viewer')),
